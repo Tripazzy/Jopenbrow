311577c31e917527ee44e89e97d2dc0262d5b184
TheOnlyMrCat <signupforforums5@gmail.com>
2018-11-24 19:46:53 +1100

    Converted data file to java properties format

--- src/salsaboy/jopenbrow/Jopenbrow.java
+++ src/salsaboy/jopenbrow/Jopenbrow.java
@@ -1,28 +1,19 @@
 package salsaboy.jopenbrow;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.*;
 import java.net.URL;
-import java.util.Scanner;
+import java.net.URISyntaxException;
+import java.util.Properties;
 
 public class Jopenbrow {
     public static URL homepage;
     
     public static void main(String[] args) throws IOException, URISyntaxException {
-        Scanner configReader = new Scanner(new File(Jopenbrow.class.getResource("data").toURI()));
-        while (configReader.hasNext()) {
-            String full = configReader.nextLine();
-            String command = full.split(":")[0];
-            String param = full.split(":")[1] + ":" + full.split(":")[2];
-            
-            switch (command) {
-                case "homepage":
-                    homepage = new URL(param);
-                    break;
-            }
-        }
-        
+        Properties appProperties = new Properties();
+        appProperties.load(Jopenbrow.class.getResourceAsStream("config.properties"));
+
+        homepage = new URL(appProperties.getProperty("homepage"));
+
         new Window();
     }
 }
