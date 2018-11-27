4c242f242d301448ea7878ea8ba85fbabe36fb69
TheOnlyMrCat <signupforforums5@gmail.com>
2018-11-27 13:19:56 +1100

    Reindent files with tab

--- src/salsaboy/jopenbrow/Jopenbrow.java
+++ src/salsaboy/jopenbrow/Jopenbrow.java
@@ -6,14 +6,14 @@ import java.net.URISyntaxException;
 import java.util.Properties;
 
 public class Jopenbrow {
-    public static URL homepage;
-    
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        Properties appProperties = new Properties();
-        appProperties.load(Jopenbrow.class.getResourceAsStream("config.properties"));
+	public static URL homepage;
 
-        homepage = new URL(appProperties.getProperty("homepage"));
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		Properties appProperties = new Properties();
+		appProperties.load(Jopenbrow.class.getResourceAsStream("config.properties"));
 
-        new Window();
-    }
+		homepage = new URL(appProperties.getProperty("homepage"));
+
+		new Window();
+	}
 }
--- src/salsaboy/jopenbrow/Window.java
+++ src/salsaboy/jopenbrow/Window.java
@@ -6,31 +6,31 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Window extends JFrame {
-    Tab currentPage;
-    
-    public Window() {
-        super("Jopenbrow");
-        
-        currentPage = new Tab();
-        setTitle(currentPage.title);
-    
-        setSize(1115, 690);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-    
-    public Window(String site) {
-        super("Jopenbrow");
-    
-        try {
-            currentPage = new Tab(new URL(site));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        
-        setTitle(currentPage.title);
-        setSize(1115, 690);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+	Tab currentPage;
+
+	public Window() {
+		super("Jopenbrow");
+
+		currentPage = new Tab();
+		setTitle(currentPage.title);
+
+		setSize(1115, 690);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public Window(String site) {
+		super("Jopenbrow");
+
+		try {
+			currentPage = new Tab(new URL(site));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+		setTitle(currentPage.title);
+		setSize(1115, 690);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
