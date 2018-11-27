b6386a27ae3b5242c8e52c114c885a755974c5c3
TheOnlyMrCat <signupforforums5@gmail.com>
2018-11-27 17:49:05 +1100

    Changed the homepage from a URL to a URI

--- src/salsaboy/jopenbrow/Jopenbrow.java
+++ src/salsaboy/jopenbrow/Jopenbrow.java
@@ -1,18 +1,18 @@
 package salsaboy.jopenbrow;
 
 import java.io.*;
-import java.net.URL;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
 public class Jopenbrow {
-	public static URL homepage;
+	public static URI homepage;
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		Properties appProperties = new Properties();
 		appProperties.load(Jopenbrow.class.getResourceAsStream("config.properties"));
 
-		homepage = new URL(appProperties.getProperty("homepage"));
+		homepage = new URI(appProperties.getProperty("homepage"));
 
 		new Window();
 	}
