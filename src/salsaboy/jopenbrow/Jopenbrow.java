f932e6ad35ede648aaa8a42e1f994af4e2e81c43
Salsa Boy <fielddashos@gmail.com>
2019-01-16 11:46:47 +1100

    Updated to JEditorPane stuff

--- src/salsaboy/jopenbrow/Jopenbrow.java
+++ src/salsaboy/jopenbrow/Jopenbrow.java
@@ -1,28 +1,12 @@
 package salsaboy.jopenbrow;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
 
-public class Jopenbrow {
-    public static URL homepage;
-    
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        Scanner configReader = new Scanner(new File(Jopenbrow.class.getResource("data").toURI()));
-        while (configReader.hasNext()) {
-            String full = configReader.nextLine();
-            String command = full.split(":")[0];
-            String param = full.split(":")[1] + ":" + full.split(":")[2];
-            
-            switch (command) {
-                case "homepage":
-                    homepage = new URL(param);
-                    break;
-            }
-        }
-        
-        new Window();
+public class Jopenbrow
+{
+    public static void main(String[] args) throws MalformedURLException
+    {
+        new Window(new URL(Window.homepage));
     }
 }
--- src/salsaboy/jopenbrow/Window.java
+++ src/salsaboy/jopenbrow/Window.java
@@ -1,36 +1,117 @@
 package salsaboy.jopenbrow;
 
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Window extends JFrame {
-    Tab currentPage;
-    
-    public Window() {
-        super("Jopenbrow");
-        
-        currentPage = new Tab();
-        setTitle(currentPage.title);
-    
-        setSize(1115, 690);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-    
-    public Window(String site) {
-        super("Jopenbrow");
-    
-        try {
-            currentPage = new Tab(new URL(site));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        
-        setTitle(currentPage.title);
-        setSize(1115, 690);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+public class Window extends JFrame implements HyperlinkListener, KeyListener
+{
+	public static String homepage = "https://www.google.com/";
+	
+	private JEditorPane page = new JEditorPane();
+	private JTextField search = new JTextField();
+	private JScrollPane scrollPane = new JScrollPane(page);
+	
+	public Window(URL url)
+	{
+		setLayout(new BorderLayout());
+		
+		try
+		{
+			page.setPage(url);
+		} catch (IOException e)
+		{
+			page.setText("404 Page not found");
+		}
+		
+		page.setEditable(false);
+		
+		search.setText(url.toString());
+		
+		add(scrollPane);
+		add(search, BorderLayout.NORTH);
+		
+		page.addHyperlinkListener(this);
+		scrollPane.addKeyListener(this);
+		
+		search.addActionListener(e ->
+		{
+			try
+			{
+				page.setPage(search.getText());
+			} catch (IOException e1)
+			{
+				page.setText("404 Page not found");
+			}
+		});
+		
+		repaint();
+		
+		setSize(1280, 720);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	private void gotoPage(URL url)
+	{
+		try
+		{
+			page.setPage(url);
+		} catch (IOException e)
+		{
+			page.setText("404 Page not found");
+		}
+		
+		repaint();
+	}
+	
+	@Override
+	public void hyperlinkUpdate(HyperlinkEvent e)
+	{
+		if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
+		{
+			gotoPage(e.getURL());
+			search.setText(e.getURL().toString());
+		}
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e)
+	{
+	}
+	
+	private boolean control, t;
+	
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+		t = e.getKeyChar() == 't';
+		control = e.getKeyCode() == KeyEvent.VK_META || e.getKeyCode() == KeyEvent.VK_CONTROL;
+		
+		if (control && t)
+		{
+			try
+			{
+				new Window(new URL(Window.homepage));
+			} catch (MalformedURLException e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		if (e.getKeyChar() == 't') t = false;
+		if (e.getKeyCode() == KeyEvent.VK_CONTROL || e.getKeyCode() == KeyEvent.VK_META) control = false;
+		
+		if (e.getKeyCode() == KeyEvent.VK_F12) page.setEditable(!page.isEditable());
+	}
 }
